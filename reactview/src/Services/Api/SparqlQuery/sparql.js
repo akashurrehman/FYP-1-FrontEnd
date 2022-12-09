18fd9c2b8542ed002a2c61226d5bda654cf337bf
akashurrehman <fa19-bse-112@cuilahore.edu.pk>
2022-12-09 17:09:36 +0500

    Editing the single file to try out git add/commit

--- reactview/src/Services/Api/SparqlQuery/sparql.js
+++ reactview/src/Services/Api/SparqlQuery/sparql.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sparql = () => {
     return ( 
         <div>
-            <h1>SPARQL Test for checking cmm</h1>
+            <h1>SPARQL Test for checking New one</h1>
         </div>
      );
 }
