051489b5ccdf22cd71aefc48149846de2b6b80cb
akashurrehman <fa19-bse-112@cuilahore.edu.pk>
2022-12-09 16:55:21 +0500

    Check commit

---
+++ reactview/src/Services/Api/SparqlQuery/sparql.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sparql = () => {
+    return ( 
+        <div>
+            <h1>SPARQL Test for checking cmm</h1>
+        </div>
+     );
+}
+ 
+export default Sparql;
