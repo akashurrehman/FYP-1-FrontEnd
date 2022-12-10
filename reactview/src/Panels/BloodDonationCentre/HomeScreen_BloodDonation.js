f03b42d7cec287cab56acee7f1b12a72a5b3bc86
akashurrehman <fa19-bse-112@cuilahore.edu.pk>
2022-12-10 14:31:35 +0500

    Routes added in App.js

---
+++ reactview/src/Panels/BloodDonationCentre/HomeScreen_BloodDonation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../../Components_for_All_Panels/BloodCentre/Footer";
+import Header from "../../Components_for_All_Panels/BloodCentre/Header";
+
+const HomeScreen_BloodDonation = () => {
+    return ( 
+        <div>
+        <Header/>
+            <h1>HomeScreen_BloodDonation</h1>
+        <Footer/>
+        </div>
+     );
+}
+
+export default HomeScreen_BloodDonation;
