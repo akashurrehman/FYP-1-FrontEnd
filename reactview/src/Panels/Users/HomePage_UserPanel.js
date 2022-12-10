a4e633375a98ee8366a31408d6f7424b5c17766c
mabuhurairah <abuhurairahishtiaq37@gmail.com>
2022-12-10 16:31:05 +0500

    User Panel hume screen file added

---
+++ reactview/src/Panels/Users/HomePage_UserPanel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserPanelFooter from "../../Components_for_All_Panels/UserComponents/UserPanelFooter";
+import UserPanelHeader from "../../Components_for_All_Panels/UserComponents/UserPanelHeader";
+
+const HomeScreen_UserPanel = () => {
+    return ( 
+        <div>
+        <UserPanelFooter/>
+        </div>
+     );
+}
+
+export default HomeScreen_UserPanel;
