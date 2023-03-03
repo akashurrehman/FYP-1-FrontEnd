e092769e104dc2b0a037cbf2ce36391988d321f8
Akash Ur Rehman <fa19-bse-112@cuilahore.edu.pk>
2023-03-03 18:18:13 +0500

    json added to Query

--- JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/HelloController.java
+++ JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.restapi.restapi.controller;
 
 import java.io.*;
+import org.springframework.http.HttpHeaders;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.restapi.restapi.entity.Person;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -20,6 +25,12 @@ import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateRequest;
+import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @RestController
 public class HelloController {
@@ -46,8 +57,15 @@ public class HelloController {
 
     // get request mapping with query parameter
     @GetMapping("/helloParam")
-    public void sparqlMethodString() {
-        SparqlTest();
+    public ResponseEntity<String> sparqlMethodString() {
+        System.out.print(SparqlTest());
+        // set the response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String result = SparqlTest();
+        // create the response object with the JSON result and headers
+        return new ResponseEntity<String>(result, HttpStatus.OK);
     }
 
     // get request mapping with path variable
@@ -69,11 +87,13 @@ public class HelloController {
     }
 
     // Pull Request for Insert Query
-    static void SparqlTest() {
+    static String SparqlTest() {
 
         // create a file object for the RDF file
-        File file = new File("D:/Final Year Project/FYP FrontEnd 2/FYP-1-FrontEnd/JavaSpring/RestAPI/src/main/resources/data/Blood.xml");
+        File file = new File(
+                "D:/Akash/Semester 7/Final Year Project/Front_End_Implementation/FYP-1-FrontEnd/JavaSpring/RestAPI/src/main/resources/data/Blood.xml");
 
+        //
         // create a model from the RDF file
         Model model = ModelFactory.createDefaultModel();
         InputStream in = null;
@@ -91,21 +111,37 @@ public class HelloController {
                 }
             }
         }
+        /*
+         * String updateString =
+         * "PREFIX ex: <http://example.com/> INSERT DATA { ex:John ex:hasAge \"25\"^^xsd:int . }"
+         * ;
+         * UpdateRequest updateRequest = UpdateFactory.create(updateString);
+         * Dataset dataset = DatasetFactory.create(model);
+         * UpdateProcessor updateProcessor =
+         * UpdateExecutionFactory.create(updateRequest, dataset);
+         * updateProcessor.execute();
+         */
 
         // create a SPARQL query
-        String queryString = 
-        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" + 
-        "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>" +
-        "SELECT * WHERE { " + 
-        "?users bd:userEnrollsIn bd:DONOR_Website " +
-        "}" ;
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>" +
+
+                "SELECT ?persons ?id ?name ?email WHERE {" +
+                "?persons rdf:type bd:Person ." +
+                "?persons bd:hasPersonID ?id ." +
+                "?persons bd:hasPersonFullName ?name ." +
+                "?persons bd:hasPersonEmail ?email ." +
+                "}";
 
         Query query = QueryFactory.create(queryString);
 
         // execute the query and print the results
         try (QueryExecution qe = QueryExecutionFactory.create(query, model)) {
             ResultSet results = qe.execSelect();
-            ResultSetFormatter.out(System.out, results, query);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ResultSetFormatter.outputAsJSON(outputStream, results);
+            String jsonResult = outputStream.toString();
+            return jsonResult;
         }
     }
 }
