a43ba308e541df664f714d9c9fae86a25fee2b5a
Akash Ur Rehman <fa19-bse-112@cuilahore.edu.pk>
2023-03-08 21:23:14 +0500

    Insert Query-Test

--- JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/User.java
+++ JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/User.java
@@ -16,7 +16,7 @@ import com.bridgelabz.restapi.restapi.entity.Person;
 
 //import org.apache.jena.query.Dataset;
 //import org.apache.jena.query.DatasetFactory;
-//import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.query.Query;
@@ -28,9 +28,9 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.update.UpdateAction;
-//import org.apache.jena.update.UpdateExecutionFactory;
-//import org.apache.jena.update.UpdateFactory;
-//import org.apache.jena.update.UpdateRequest;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateRequest;
 //import org.json.JSONArray;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -53,7 +53,7 @@ public class User {
 
     @GetMapping("/api/insert")
     public void Insert() {
-        // InsertSparql();
+        InsertSparql();
         // Insert using UpdateAction
     }
 
@@ -100,29 +100,25 @@ public class User {
                 }
             }
         }
+
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
+        "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>"+
+        
+        "SELECT ?centers ?id ?name ?email WHERE {"+
+          "?centers rdf:type bd:Blood_Donation_Center ." +
+          "?centers bd:hasCenterID ?id ."+
+          "?centers bd:hasCenterName ?name ."+
+          "?centers bd:hasCenterEmail ?email ."+
+        "}";
         /*
-         * String updateString =
-         * "PREFIX ex: <http://example.com/> INSERT DATA { ex:John ex:hasAge \"25\"^^xsd:int . }"
-         * ;
-         * UpdateRequest updateRequest = UpdateFactory.create(updateString);
-         * Dataset dataset = DatasetFactory.create(model);
-         * UpdateProcessor updateProcessor =
-         * UpdateExecutionFactory.create(updateRequest, dataset);
-         * updateProcessor.execute();
+         * String queryString =
+         * "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>"
+         * +
+         * "SELECT ?person ?bloodType WHERE {" +
+         * "?person bd:userName 'John Doe' ." +
+         * "?person bd:userBloodType ?bloodType ." +
+         * "}";
          */
-
-        // create a SPARQL query
-        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-                "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>" +
-
-                "SELECT ?persons ?id ?name ?email WHERE {" +
-                "?persons rdf:type bd:Person ." +
-                "?persons bd:hasPersonID ?id ." +
-                "?persons bd:hasPersonFullName ?name ." +
-                "?persons bd:hasPersonEmail ?email ." +
-                /* FILTER(?id = "${enteredId}") */
-                "}";
-
         Query query = QueryFactory.create(queryString);
 
         // execute the query and print the results
@@ -134,5 +130,46 @@ public class User {
             return jsonResult;
             // Returns the results in json format
         }
+    }
+
+    static void InsertSparql() {
+
+        // create a file object for the RDF file
+        File file = new File(
+                "D:/Akash/Semester 7/Final Year Project/Front_End_Implementation/FYP-1-FrontEnd/JavaSpring/RestAPI/src/main/resources/data/Blood.xml");
+
+        //
+        // create a model from the RDF file
+        Model model = ModelFactory.createDefaultModel();
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            model.read(in, null);
+        } catch (IOException e) {
+            System.out.println("No file Found!");
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // handle the exception
+                }
+            }
+        }
+
+        String updateString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX bd: <http://www.semanticweb.org/samsung/ontologies/2022/10/blood-donation-system#>" +
+
+                "INSERT DATA{" +
+                "bd:Center1 rdf:type bd:Blood_Donation_Center ." +
+                "bd:Center1 bd:hasCenterID '123' ." +
+                "bd:Center1 bd:hasCenterName 'Shukat Khanam' ." +
+                "bd:Center1 bd:hasCenterEmail 'shoukat@example.com' ." +
+                "}";
+
+        UpdateRequest updateRequest = UpdateFactory.create(updateString);
+        Dataset dataset = DatasetFactory.create(model);
+        UpdateProcessor updateProcessor = UpdateExecutionFactory.create(updateRequest, dataset);
+        updateProcessor.execute();
     }
 }
