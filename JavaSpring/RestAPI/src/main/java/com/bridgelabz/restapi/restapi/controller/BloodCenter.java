651a3d0ba832ae9e42826d2ca5e78eec9ffc84c7
Akash Ur Rehman <fa19-bse-112@cuilahore.edu.pk>
2023-03-09 22:09:54 +0500

    Routes added

--- JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/BloodCenter.java
+++ JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/BloodCenter.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.restapi.restapi.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,18 @@ public class BloodCenter {
         return "Blood Donation Center";
     }
 
-    @GetMapping("/api/blood")
+    @GetMapping("/api/bloodCenter")
     public String blood() {
+        return "Blood Group: A+";
+    }
+
+    @DeleteMapping("/api/bloodCenter/{id}")
+    public String center(@RequestBody String id) {
+        return "Delete Route";
+    }
+
+    @PostMapping("/api/bloodCenter")
+    public String bloodPost(@RequestBody String blood) {
         return "Blood Group: A+";
     }
 }
