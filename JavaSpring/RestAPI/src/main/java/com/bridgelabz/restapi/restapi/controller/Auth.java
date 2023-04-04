a36cb5160308396bcba85ea3b9245859d5eaca4e
Akash Ur Rehman <fa19-bse-112@cuilahore.edu.pk>
2023-04-04 14:30:33 +0500

    Pull Request by Akash

--- JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/Auth.java
+++ JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/Auth.java
@@ -1,8 +1,6 @@
 package com.bridgelabz.restapi.restapi.controller;
 
 import org.springframework.http.ResponseEntity;
-// import org.apache.jena.query.*;
-// import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 
@@ -11,17 +9,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
-// import java.security.InvalidKeyException;
-// import java.security.NoSuchAlgorithmException;
-// import java.security.SignatureException;
 
 import org.springframework.http.HttpHeaders;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -217,5 +207,4 @@ public class Auth {
             // Returns the results in json format
         }
     }
-
 }
--- JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/User.java
+++ JavaSpring/RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/User.java
@@ -28,7 +28,6 @@ import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateRequest;
 import org.json.JSONArray;
 import org.json.JSONObject;
-//import org.json.JSONArray;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -196,27 +195,28 @@ public class User {
 
         if (bindingsArr.isEmpty()) {
             String query = String.format(
-                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                        "PREFIX bd: <http://www.semanticweb.org/mabuh/ontologies/2023/blood_donation_system#>\n" +
-                        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n\n" +
-                        "INSERT DATA {\n" +
-                        "bd:" + individualId + " rdf:type bd:Person ;\n" +
-                        "   rdf:type bd:Donor ;\n" +
-                        "   rdf:type bd:Request_Maker ;\n" +
-                        "                       bd:hasPersonFullName \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasUserName \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPassword \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasRole \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonCity \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonBloodGroup \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonAddress \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonContactNo \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonEmail \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonGender \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonDateOfBirth \"%s\"^^xsd:string ;\n" +
-                        "                       bd:hasPersonID \"%s\"^^xsd:string ;\n" +
-                        "}",
-                fullName, userName, password, role, city, bloodGroup, address, contactNo, email, gender, dob, individualId);
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                            "PREFIX bd: <http://www.semanticweb.org/mabuh/ontologies/2023/blood_donation_system#>\n" +
+                            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n\n" +
+                            "INSERT DATA {\n" +
+                            "bd:" + individualId + " rdf:type bd:Person ;\n" +
+                            "   rdf:type bd:Donor ;\n" +
+                            "   rdf:type bd:Request_Maker ;\n" +
+                            "                       bd:hasPersonFullName \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasUserName \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPassword \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasRole \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonCity \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonBloodGroup \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonAddress \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonContactNo \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonEmail \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonGender \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonDateOfBirth \"%s\"^^xsd:string ;\n" +
+                            "                       bd:hasPersonID \"%s\"^^xsd:string ;\n" +
+                            "}",
+                    fullName, userName, password, role, city, bloodGroup, address, contactNo, email, gender, dob,
+                    individualId);
 
             // Call the InsertSparql function with the query
             boolean isInserted = InsertSparql(query);
@@ -225,13 +225,13 @@ public class User {
                 String successMessage = "{\"success\": \"Data inserted successfully\"}";
                 return new ResponseEntity<String>(successMessage, HttpStatus.OK);
             } else {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while inserting data");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error occurred while inserting data");
             }
-        }
-        else{
+        } else {
             String errorMessage = "{\"error\": \"User with this username and email already exit: " + userName + "\"}";
             return new ResponseEntity<String>(errorMessage, headers, HttpStatus.NOT_FOUND);
-        }        
+        }
     }
 
     /*
