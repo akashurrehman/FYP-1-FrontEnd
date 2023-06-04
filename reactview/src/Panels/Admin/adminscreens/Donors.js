a4609d56fde7a320ac17baed6cd3505e7baec6b3
drsalmanahmed07 <102178798+drsalmanahmed07@users.noreply.github.com>
2023-06-04 21:36:39 +0500

    HardCoded Maps Added
    Hard Coded Maps

--- reactview/src/Panels/Admin/adminscreens/Donors.js
+++ reactview/src/Panels/Admin/adminscreens/Donors.js
@@ -4,23 +4,25 @@ import axios from "axios";
 import "../adminscreen.css";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
   Trash,
 } from "react-bootstrap-icons";
 import { useAuth } from "../../BloodDonationCentre/Auth/AuthContext";
 import jwtDecode from "jwt-decode";
-import { Button, Container, Dropdown, DropdownButton, Form, Nav, Row } from "react-bootstrap";
-
+import { Button, Dropdown, DropdownButton } from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal';
 export default function Donors() {
+
+
   const { token } = useAuth();
 
   const decodedToken = token ? jwtDecode(token) : null;
   const role = decodedToken?.role;
 
   const authCentre = () => {
-    if (role != 'ADMIN') {
+    if (role !== 'ADMIN') {
       window.location.href = "/user/login";
     }
     console.log("authCentre");
@@ -64,7 +66,9 @@ export default function Donors() {
       });
   }, []);
 
-
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const generatePDF = () => {
     const doc = new jsPDF();
@@ -135,120 +139,131 @@ export default function Donors() {
 
 
   return (
-    <div className="turningred fontfamily">
-      <div className="pdf-campaigns-container" ref={pdfContainerRef}>
-        <div className="buttonInDonor">
-          <h1 className="color">Donors List</h1><br />
-
-
-
-
-          <button
-            className="btn btn-danger BtnGeneratePDF "
-            onClick={generatePDF}
-          >
-            Generate PDF
-          </button>
-        </div>
-
-        <div className="row mt-4">
-          <div className="col-lg-2 col-12"> <h3>Filters:</h3></div>
-          <div className="col-lg-6 gap col-12">
-
-            <DropdownButton
-              id="dropdown-item-button"
-              // className="custom-dropdown-button"
-              title={selectedBloodGroup || "Select Blood Group"}
-              onSelect={(bloodGroup) => setSelectedBloodGroup(bloodGroup)}
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Location</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><iframe title="Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27239.569487006618!2d74.19039984886317!3d31.415608720761064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3918fe2c620a57e5%3A0x542f94f8ded04ba8!2sLDA%20Avenue%20One%2C%20Lahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1685896289913!5m2!1sen!2s" style={{width:"100%", height:"300px"  }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div className="turningred fontfamily">
+        <div className="pdf-campaigns-container" ref={pdfContainerRef}>
+          <div className="buttonInDonor">
+            <h1 className="color">Donors List</h1><br />
+            <button
+              className="btn btn-danger BtnGeneratePDF "
+              onClick={generatePDF}
             >
-              {bloodArray.map((bloodGroup, index) => (
-                <Dropdown.Item key={index} eventKey={bloodGroup}>
-                  {bloodGroup}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
-            <DropdownButton
-              id="dropdown-item-button"
-              title={selectedCity || "Select City"}
-              onSelect={(city) => setSelectedCity(city)}
-            >
-              {cityArray.map((city, index) => (
-                <Dropdown.Item key={index} eventKey={city}>
-                  {city}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
-            {filtersApplied && (
-
-              <button className="btn btn-secondary icnss" onClick={clearFilters}>
-                <Trash className="trashbox" size={18} />
-              </button>
-            )}
-
+              Generate PDF
+            </button>
           </div>
-        </div>
-        <div className="cardsmapping mt-2">
-          {filteredDonors.map((faq, index) => (
-            <div className="headin" key={index}>
-              <div className="card DonoCard">
-                <div className="card-body">
-                  <h5 className="card-title"> {faq.name}</h5>
-                  <h6 className="card-subtitle mb-2 text-muted"> {faq.id}</h6>
 
-                  <h5 className="card-title"> Details:</h5>
-                  <p className="card-text">
-                    <div className="row">
-                      <div className="col-lg-9 col-12">
-                        <p className="m-1">
+          <div className="row mt-4">
+            <div className="col-lg-2 col-12"> <h3>Filters:</h3></div>
+            <div className="col-lg-6 gap col-12">
+
+              <DropdownButton
+                id="dropdown-item-button"
+                // className="custom-dropdown-button"
+                title={selectedBloodGroup || "Select Blood Group"}
+                onSelect={(bloodGroup) => setSelectedBloodGroup(bloodGroup)}
+              >
+                {bloodArray.map((bloodGroup, index) => (
+                  <Dropdown.Item key={index} eventKey={bloodGroup}>
+                    {bloodGroup}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+              <DropdownButton
+                id="dropdown-item-button"
+                title={selectedCity || "Select City"}
+                onSelect={(city) => setSelectedCity(city)}
+              >
+                {cityArray.map((city, index) => (
+                  <Dropdown.Item key={index} eventKey={city}>
+                    {city}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+              {filtersApplied && (
+
+                <button className="btn btn-secondary icnss" onClick={clearFilters}>
+                  <Trash className="trashbox" size={18} />
+                </button>
+              )}
+
+            </div>
+          </div>
+          <div className="cardsmapping mt-2">
+            {filteredDonors.map((faq, index) => (
+              <div className="headin" key={index}>
+                <div className="card DonoCard">
+                  <div className="card-body">
+                    <h5 className="card-title"> {faq.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted"> {faq.id}</h6>
+
+                    <h5 className="card-title"> Details:</h5>
+                    <p className="card-text">
+                      <div className="row">
+                        <div className="col-lg-9 col-12">
+                          <p className="m-1">
+                            {" "}
+                            <strong>Email: </strong>
+                            {faq.email}
+                          </p>
+                        </div>
+                        <div className="col-lg-3 col-12">
                           {" "}
-                          <strong>Email: </strong>
-                          {faq.email}
-                        </p>
+                          <strong> {faq.gender}</strong>
+                        </div>
                       </div>
-                      <div className="col-lg-3 col-12">
-                        {" "}
-                        <strong> {faq.gender}</strong>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-lg-9 col-12">
-                        <p>
+                      <div className="row">
+                        <div className="col-lg-9 col-12">
+                          <p>
+                            {" "}
+                            <strong>Contact: </strong>
+                            {faq.contact}{" "}
+                          </p>
+                        </div>
+                        <div className="col-lg-3 col-12">
                           {" "}
-                          <strong>Contact: </strong>
-                          {faq.contact}{" "}
-                        </p>
+                          <strong className="red"> {faq.bloodGroup}</strong>
+                        </div>
                       </div>
-                      <div className="col-lg-3 col-12">
-                        {" "}
-                        <strong className="red"> {faq.bloodGroup}</strong>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-12">
-                        <h6 className="card-subtitle mb-2 text-muted">
-                          {faq.location}{" "}
-                        </h6>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-12">
-                        <strong>City: </strong> {faq.city}{" "}
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-12 ">
-                        <strong >Message:</strong> <br />
-                        <span className="text-muted">  {faq.message}{" "}</span>
+                      <div className="row">
+                        <div onClick={handleShow} className="col-12 cursorpointer">
+                          <h6 className="card-subtitle mb-2 text-muted">
+                            {faq.location}{" "}
+                          </h6>
+                        </div>
 
                       </div>
-                    </div>
-                  </p>
+                      <div className="row">
+                        <div className="col-12">
+                          <strong>City: </strong> {faq.city}{" "}
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-12 ">
+                          <strong >Message:</strong> <br />
+                          <span className="text-muted">  {faq.message}{" "}</span>
+
+                        </div>
+                      </div>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
+
   );
 }
